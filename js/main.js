03860d4aa4855f3b50827b2dbb80caaf89b285ec
Vycheslav <98029304+Smurtan@users.noreply.github.com>
2023-02-01 19:35:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(function(){
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        fade: true,
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                dots: false
+            }
+        }]
+    });
+
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1140,
+            settings: {
+                slidesToShow: 3
+            }},
+            {
+                breakpoint: 890,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 586,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active')
+    })
+});
